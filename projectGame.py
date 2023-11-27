fdd43fad1c996a76a2199b2de3e4e292544b81d1
AqilzFracs <mahdiagilali@gmail.com>
2023-11-27 16:31:38 +0700

    fix bug

--- projectGame.py
+++ projectGame.py
@@ -49,7 +49,7 @@ ball_types = [
     {'image': bola, 'size': 30, 'speed': 2, 'is_bomb': False},
     {'image': bomb, 'size': 30, 'speed': 2, 'is_bomb': True}
 ]
-num_ball = 5
+num_ball = 3
 num_bombs = 2
 
 ball_list = [
@@ -58,48 +58,23 @@ ball_list = [
         'x': random.randint(0, width - 30),
         'y': random.randint(-50, 0),  # Koordinat awal di luar layar
         'reset_ball': {'x': random.randint(0, width - 30), 'y': random.randint(-50, 0)}
-    } for _ in range(num_ball - num_bombs)
+    } for i in range(num_ball   - num_bombs)
 ]
 
-for _ in range(num_bombs):
+for i in range(num_bombs):
     ball_list.append({
         'type': random.choice(ball_types),
         'x': random.randint(0, width - 30),
-        'y': random.randint(-50, 0),
-        'reset_ball': {'x': random.randint(0, width - 30), 'y': random.randint(-50, 0)}
+        'y': random.randint(-15, 0),
+        'reset_ball': {'x': random.randint(0, width - 30), 'y': random.randint(-15, 0)}
     })
-# basketBall_size = 30
-# basketBall_x = random.randint(0, width - basketBall_size)
-# basketBall_y = 400
-# basketBall_speed = 5
-
-# footBall_size = 30
-# footBall_x = random.randint(0, width - footBall_size)
-# footBall_y = 400
-# footBall_speed = 5
-
-# volleyBall_size = 30
-# volleyBall_x = random.randint(0, width - volleyBall_size)
-# volleyBall_y = 400
-# volleyBall_speed = 5
-
-# bomb_size = 30
-# bomb_x = random.randint(0, width - bomb_size)
-# bomb_y = 400
-# bomb_speed = 5
 
 # Skor
 score = 0
 font = pygame.font.SysFont(None, 30)
 
 # nyawa
-lives = 5
-
-
-
-# ground_scroll = 0
-# ground_scroll_x2 = 600
-# scroll_speed = 3
+lives = 100
 
 def button_MM():
     glPushMatrix()
@@ -191,15 +166,19 @@ def draw_text(text, x, y):
     pygame.display.get_surface().blit(render_text, (x, y))
 
 def resetgame():
-    global ring_x, ball_list, ring_speed, lives
+    global ring_x, ball_list, ring_speed, lives, score
+    score = 0
+    lives = 5
+    for ball in ball_list:
+        ball['type']['speed'] = 2
     ring_x = width // 2 - ring_size // 2
     # Tambahan: Atur ulang posisi makanan jika ada
     ball_list = [
       { 
           'type': random.choice(ball_types),
           'x': random.randint(0, width - 30),
-          'y': random.randint(-50, 0),  # Koordinat awal di luar layar
-          'reset_ball': {'x': random.randint(0, width - 30), 'y': random.randint(-50, 0)}
+          'y': random.randint(-15, 0),  # Koordinat awal di luar layar
+          'reset_ball': {'x': random.randint(0, width - 30), 'y': random.randint(-15, 0)}
       } for _ in range(num_ball - num_bombs)
     ]
 
@@ -294,7 +273,8 @@ while run:
           size = ball['type']['size']
           if ball['y'] > height:
             if not ball['type']['is_bomb'] :
-                ball['x'], ball['y'] = ball['reset_ball']['x'], ball['reset_ball']['y']
+                ball['x'] = random.randint(0, width - 30)
+                ball['y'] = random.randint(-15, 0)
                 ball['type'] = random.choice(ball_types)
                 lives -= 1
                 if lives == 0:
@@ -302,6 +282,15 @@ while run:
                     start = False
                     show_menu = True
                     resetgame()
+            else:
+                if ball['type']['speed'] -2 <= 4:
+                    ball['type']['speed'] = 2
+                else:
+                    ball['type']['speed'] -= 2
+                ball['x'] = random.randint(0, width - 30)
+                ball['y'] = random.randint(-15, 0)
+                ball['type'] = random.choice(ball_types)
+
           elif (
                 ring_x < ball['x'] + size
                 and ring_x + ring_size > ball['x']
@@ -313,7 +302,8 @@ while run:
                       ball['type']['speed'] = 2
                   else:
                       ball['type']['speed'] -= 2
-                  ball['x'], ball['y'] = ball['reset_ball']['x'], ball['reset_ball']['y']
+                  ball['x'] = random.randint(0, width - 30)
+                  ball['y'] = random.randint(-15, 0)
                   ball['type'] = random.choice(ball_types)
                   lives -= 1
                   if lives == 0:
@@ -325,23 +315,25 @@ while run:
                   score += 1
                   if score % 10 == 0:
                       for ball in ball_list:
-                          ball['type']['speed'] += 2
-                  ball['x'], ball['y'] = ball['reset_ball']['x'], ball['reset_ball']['y']
+                          ball['type']['speed'] += 1
+                  ball['x'] = random.randint(0, width - 30)
+                  ball['y'] = random.randint(-15, 0)
                   ball['type'] = random.choice(ball_types)
               
             
     glClear(GL_COLOR_BUFFER_BIT)
 
     if start == True:
-        draw_text(f"Skor    : {score}", 10, 10)
-        draw_text(f"Nyawa : {lives}", 10, 30)
-        draw_text(f"Speed : {ring_speed}", 10, 50)
+        for ball in ball_list:
+            draw_text(f"Skor  : {score}", 10, 10)
+            draw_text(f"Nyawa : {lives}", 10, 30)
+            draw_text(f"Speed : {ball['type']['speed']}", 10, 50)
 
     glEnable(GL_TEXTURE_2D)
     glBindTexture(GL_TEXTURE_2D, glGenTextures(1))
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pygame.image.tostring(pygame.display.get_surface(), 'RGBA'))
-    # draw_ground()
+    draw_ground()
     draw_background()
 
     glDisable(GL_TEXTURE_2D)
